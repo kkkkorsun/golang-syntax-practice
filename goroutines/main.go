2651ab7a45486fe3283608f33613f188aba12507
Pavel Korsun <korsun.dev@gmail.com>
2026-03-30 23:17:49 +0300

    concurrency practice

--- goroutines/main.go
+++ goroutines/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
 	"strconv"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -16,7 +19,19 @@ func main() {
 
 	//fourthTask()
 
-	fifthTask()
+	//fifthTask()
+
+	//sixthTask()
+
+	//seventhTask()
+
+	//eighthTask()
+
+	//ninthTask()
+
+	//tenthTask()
+
+	eleventhTask()
 }
 
 func firstTask() {
@@ -142,4 +157,221 @@ func fifthTask() {
 	default:
 		fmt.Println("К моменту чтения ничего не было записано в канал")
 	}
+}
+
+func sixthTask() {
+	//var chInt chan int
+	//writeToNilChannel(chInt)
+	//readFromNilChannel(chInt)
+
+	nonBufferedChan := make(chan int)
+
+	go func(ch chan int) {
+		ch <- 1
+		close(ch)
+	}(nonBufferedChan)
+	//close(nonBufferedChan)
+
+	for {
+		if v, ok := <-nonBufferedChan; ok {
+			fmt.Println(v)
+		}
+	}
+
+}
+
+func writeToNilChannel(chInt chan<- int) {
+	chInt <- rand.Int()
+}
+
+func readFromNilChannel(chInt <-chan int) {
+	fmt.Println("Попытка чтения из канала", <-chInt)
+}
+
+func seventhTask() {
+	peopleOpinions := make(chan string)
+
+	peopleCount := rand.Intn(10)
+	fmt.Println("Количество человек:", peopleCount)
+
+	delays := []int{300, 400, 500, 600, 700}
+	delay := delays[rand.Intn(len(delays))]
+	fmt.Println("Задержка:", delay)
+
+	go func(ch chan<- string) {
+		defer close(ch)
+
+		for i := 0; i < peopleCount; i++ {
+			time.Sleep(time.Duration(delay) * time.Millisecond)
+			ch <- GenerateString(5)
+		}
+	}(peopleOpinions)
+
+	for opinion := range peopleOpinions {
+		fmt.Println(opinion)
+	}
+}
+func GenerateString(length int) string {
+
+	const charset = "abcdefghijklmnopqrstuvwxyz" +
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	var seededRand *rand.Rand = rand.New(
+		rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func eighthTask() {
+	parentCxt, parentCancel := context.WithCancel(context.Background())
+	middleCxt, middleCancel := context.WithCancel(parentCxt)
+	childCxt, childCancel := context.WithCancel(middleCxt)
+
+	go childFunc(childCxt)
+	go middleFunc(middleCxt)
+	go parentFunc(parentCxt)
+
+	time.Sleep(2 * time.Second)
+	parentCancel()
+
+	time.Sleep(3 * time.Second)
+	childCancel()
+
+	time.Sleep(1 * time.Second)
+	middleCancel()
+
+}
+
+func parentFunc(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Завершили parentFunc")
+			return
+		default:
+			fmt.Println("Выполняется parentFunc")
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func middleFunc(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Завершили middleFunc")
+			return
+		default:
+			fmt.Println("Выполняется middleFunc")
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func childFunc(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Завершили childFunc")
+			return
+		default:
+			time.Sleep(1 * time.Second)
+			fmt.Println("Выполняется childFunc")
+		}
+	}
+}
+
+func ninthTask() {
+	delays := []int{500, 600, 700, 800, 900, 1000}
+
+	workersCount := rand.Intn(10) + 1
+	fmt.Println("Количество работников: ", workersCount)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < workersCount; i++ {
+		wg.Add(1)
+		go WaterTheGround(i, delays[rand.Intn(len(delays))], &wg)
+	}
+
+	wg.Wait()
+	fmt.Println("Все рабочие завершили свою работу")
+
+}
+
+func WaterTheGround(id int, delay int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Println("Огородник: ", id, "Ушел выполнять свою работу на: ", delay, "мс")
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+}
+
+func tenthTask() {
+
+	//var voicesInElectionsCount int
+	var voicesInElectionsCountAtomic atomic.Int32
+
+	var wg sync.WaitGroup
+	//var mtx sync.Mutex
+
+	electorsCount := rand.Intn(10000) + 1
+	fmt.Println("Ожидалось голосов: ", electorsCount)
+
+	for i := 0; i < electorsCount; i++ {
+		wg.Add(1)
+
+		go func(id int, wg *sync.WaitGroup) {
+			defer wg.Done()
+			voicesInElectionsCountAtomic.Add(1)
+
+			//mtx.Lock()
+			//voicesInElectionsCount++
+			//mtx.Unlock()
+
+			//fmt.Println("Избиратель: ", id, " проголосовал")
+		}(i, &wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("Получено голосов: ", voicesInElectionsCountAtomic.Load())
+}
+
+func eleventhTask() {
+	var lettersFromSubscribers []string
+	var wg sync.WaitGroup
+	var mtx sync.Mutex
+
+	subsCount := rand.Intn(100000) + 1
+	fmt.Println("Ожидаемое количество писем", subsCount)
+
+	for i := 0; i < subsCount; i++ {
+
+		wg.Add(1)
+
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+
+			mtx.Lock()
+			lettersFromSubscribers = append(lettersFromSubscribers, GenerateString(5))
+			mtx.Unlock()
+
+		}(&wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("Получено писем: ", len(lettersFromSubscribers))
+
+}
+
+func twelfthTask() {
+	keystoreMap := map[string]string{}
+	
 }
